2aefd950aa845537723a18a821ef1e46f790bb84
Drew Harris <harrisd@smu.edu>
2023-09-21 00:36:31 -0500

    Set up bot

---
+++ Dockerfile
@@ -0,0 +1,22 @@
+FROM node:18
+
+# Create app directory
+WORKDIR /app
+
+# Install app dependencies
+COPY package*.json ./
+COPY yarn.lock ./
+RUN yarn install --frozen-lockfile
+
+# Bundle app source
+COPY . .
+
+# Build app
+RUN yarn build
+
+# Expose port
+EXPOSE 3000
+
+# Start app
+CMD [ "yarn", "serve" ]
+
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,30 @@
-console.log("test");
+import { Client, Events, GatewayIntentBits } from "discord.js";
+import "dotenv/config"; // Load environment variables
+console.log(process.env.TOKEN);
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+});
+
+client.once(Events.ClientReady, (c) => {
+  console.log(`Ready! Logged in as ${c.user.tag}`);
+});
+
+client.on(Events.MessageCreate, (message) => {
+  if (message.content === "test1123") {
+    message.reply("tost1123");
+  }
+});
+
+const token = process.env.TOKEN;
+
+if (!token) {
+  console.error("No token provided!");
+  process.exit(1);
+}
+
+client.login(token);

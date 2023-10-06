2ff0a13ce9b1bc734bdce43aee1f7c034adae634
Drew Harris <harrisd@smu.edu>
2023-10-06 16:38:54 -0500

    Checkpoint

---
+++ src/commands.ts
@@ -0,0 +1,70 @@
+import {
+  CommandInteraction,
+  SlashCommandBuilder,
+  SlashCommandSubcommandsOnlyBuilder,
+} from "discord.js";
+import { z, ZodSchema, ZodType } from "zod";
+
+type SlashOptionsOnlyCommand = Omit<
+  SlashCommandBuilder,
+  "addSubcommand" | "addSubcommandGroup"
+>;
+
+const schema = z.object({});
+
+type Options = {
+  description: string;
+  type: ZodType;
+};
+
+type Command<T extends ZodType | undefined> = {
+  handle: (
+    interaction: CommandInteraction & {
+      input: T extends ZodType ? z.infer<T> : undefined;
+    },
+  ) => void;
+  command:
+    | SlashCommandBuilder
+    | SlashCommandSubcommandsOnlyBuilder
+    | SlashOptionsOnlyCommand;
+
+  schema: T;
+};
+
+export const createCommand = <T extends ZodType | undefined>(
+  handler: Command<T>["handle"],
+  schema: T,
+  options: Record<string, Options>,
+) => {
+  const b = new SlashCommandBuilder();
+
+  return {
+    handle: handler,
+    command: new SlashCommandBuilder(),
+    schema,
+  } satisfies Command<T>;
+};
+
+createCommand(
+  (interaction) => {
+    switch (interaction.input.platform) {
+      case "ps4":
+        console.log(interaction.input.platform);
+        return;
+      case "pc":
+        console.log(interaction.input.platform);
+    }
+  },
+  z.object({
+    name: z.string().min(5),
+    desc: z.string().nonempty(),
+
+    platform: z.enum(["ps4", "pc"]),
+  }),
+  {
+    name: {
+      description: "your name",
+      type: z.string(),
+    },
+  },
+);
--- src/db/index.ts
+++ src/db/index.ts
@@ -1,6 +1,6 @@
 import { drizzle } from "drizzle-orm/mysql2";
-import * as schema from "./schema";
 import mysql from "mysql2/promise";
+import * as schema from "./schema";
 
 const connection = mysql.createPool({
   uri: process.env.DB_URL,

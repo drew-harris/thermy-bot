dd0ba3459588f6b22570c7222baef5215cd81916
Drew Harris <harrisd@smu.edu>
2023-10-06 17:19:25 -0500

    major checkpoint

--- src/commands.ts
+++ src/commands.ts
@@ -1,8 +1,4 @@
-import {
-  CommandInteraction,
-  SlashCommandBuilder,
-  SlashCommandSubcommandsOnlyBuilder,
-} from "discord.js";
+import { CommandInteraction, SlashCommandBuilder } from "discord.js";
 import { z, ZodSchema, ZodType } from "zod";
 
 type SlashOptionsOnlyCommand = Omit<
@@ -11,78 +7,73 @@ type SlashOptionsOnlyCommand = Omit<
 >;
 
 type Options = {
+  name?: string;
   description: string;
   type: ZodType;
 };
 
 type OptionsList = Record<string, Options>;
 
-type Command<T extends ZodType | undefined> = {
+type Command<T> = {
   handle: (
     interaction: CommandInteraction & {
-      input: T extends ZodType ? z.infer<T> : undefined;
+      input: T;
     },
   ) => void;
-  command:
-    | SlashCommandBuilder
-    | SlashCommandSubcommandsOnlyBuilder
-    | SlashOptionsOnlyCommand;
-
-  schema: T;
+  command: SlashOptionsOnlyCommand;
+  schema: ZodSchema;
 };
-
-export const createCommand = <T extends ZodType | undefined>(
-  handler: Command<T>["handle"],
-  schema: T,
-  options: Record<string, Options>,
-) => {
-  const b = new SlashCommandBuilder();
-
-  return {
-    handle: handler,
-    command: new SlashCommandBuilder(),
-    schema,
-  } satisfies Command<T>;
-};
-
-createCommand(
-  (interaction) => {
-    switch (interaction.input.platform) {
-      case "ps4":
-        console.log(interaction.input.platform);
-        return;
-      case "pc":
-        console.log(interaction.input.platform);
-    }
-  },
-  z.object({
-    name: z.string().min(5),
-    desc: z.string().nonempty(),
-
-    platform: z.enum(["ps4", "pc"]),
-  }),
-  {
-    name: {
-      description: "your name",
-      type: z.string(),
-    },
-  },
-);
 
 type FlattenType<T> = T extends Record<string, Options> ? {
     [K in keyof T]: T[K]["type"] extends z.Schema<infer U> ? U : never;
   }
   : T;
 
-const input: OptionsList = {
-  name: {
-    description: "your name",
-    type: z.string(),
-  },
-  age: {
-    description: "your age",
-    type: z.number(),
-  },
+export const createCommand = <T extends OptionsList, F extends FlattenType<T>>(
+  handler: Command<FlattenType<T>>["handle"],
+  options: T,
+) => {
+  const b = new SlashCommandBuilder();
+
+  // Create zod schema
+  for (const [key, value] of Object.entries(options)) {
+    // If it is a string
+    if (value.type instanceof z.ZodString || value.type instanceof z.ZodEnum) {
+      b.addStringOption((o) => {
+        o.setName(value.name || key).setDescription(value.description);
+        if (value.type.isOptional()) {
+          o.setRequired(false);
+        }
+        return o;
+      });
+    }
+  }
+
+  const schema = z.object(
+    Object.fromEntries(
+      Object.entries(options).map(([key, value]) => [
+        key,
+        value.type,
+      ]),
+    ),
+  ) as unknown as ZodSchema<FlattenType<T>>;
+
+  return {
+    handle: handler,
+    schema,
+    command: b,
+  } satisfies Command<F>;
 };
 
-type FlattenedInput = FlattenType<typeof input>;
+export const statsCmd = createCommand(
+  (interaction) => {
+    console.log(interaction.input.platform);
+  },
+  // Input goes here
+  {
+    platform: {
+      description: "the platform for the account",
+      type: z.enum(["psn", "pc"]).optional(),
+    },
+  },
+);
